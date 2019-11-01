d952a0e019b37980f6d2af9125b84ddf52cb3f0e
linshizhi0848 <linshizhi@szgcom.com>
2019-11-01 18:35:19 +0800

    Provide oem register page

---
+++ manager/misc/oemRegister.py
@@ -0,0 +1,17 @@
+# oemRegister.py
+#
+# Handle of register of oem file
+import typing
+import os
+
+def oemRegister(oemIdent: str, oemFile: typing.TextIO) -> bool:
+    oemPath = "/tmp/registeredOem/" + oemIdent
+    # Is the oem already registered
+    if not os.access(oemPath, os.F_OK):
+        return False
+
+    # Register oem file
+    with open(oemPath, "w+") as dest:
+        for chunk in oemFile:
+            dest.write(chunk)
+    return True
