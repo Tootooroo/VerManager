a75189daaea56e899075c85f51c7af4de65570cd
linshizhi0848 <linshizhi@szgcom.com>
2020-04-01 11:38:45 +0800

    Remove RUN_MAIN check for master.

--- manager/apps.py
+++ manager/apps.py
@@ -23,12 +23,10 @@ class ManagerConfig(AppConfig):
         from manager.basic.info import Info
         import manager.master.master as S
 
-        if os.environ.get('RUN_MAIN') == 'true':
+        info = Info("./config.yaml")
+        sInst = S.ServerInst(info.getConfig("Address"),
+                                info.getConfig("Port"),
+                                "./config.yaml")
+        sInst.start()
 
-            info = Info("./config.yaml")
-            sInst = S.ServerInst(info.getConfig("Address"),
-                                 info.getConfig("Port"),
-                                 "./config.yaml")
-            sInst.start()
-
-            S.ServerInstance = sInst
+        S.ServerInstance = sInst
