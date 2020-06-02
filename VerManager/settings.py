7ca6ee87939c227dccad41ce65ac65337ad0574f
Tootooroo <aydenlin@foxmail.com>
2020-06-02 19:24:30 +0800

    Get allowed_hosts from env.

--- VerManager/settings.py
+++ VerManager/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '6uler=ay#_%8&f5d!_8ktc^la6i096g&q%_w$x(#7%1v1u3)hc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+HOST = os.environ.get('ALLOWED_HOST')
+if HOST is None:
+    HOST = 'localhost'
+ALLOWED_HOSTS = [HOST]
 
 
 # Application definition
