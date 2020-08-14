2932451f697c5a8c8bc314f2c2078a4fc31fb621
Tootooroo <aydenlin@foxmail.com>
2020-08-14 09:14:33 +0800

    Add license headers.

--- manager/master/master.py
+++ manager/master/master.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2020 Gcom Tootooroo
+# Copyright (c) 2020 Gcom Gcom
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
