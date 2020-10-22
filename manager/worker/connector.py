f9507431dbeb9b6dffd452248588c4a0c9b3a2e7
Tootooroo <aydenlin@foxmail.com>
2020-10-22 11:32:34 +0800

    Remove debug print.

--- manager/worker/connector.py
+++ manager/worker/connector.py
@@ -146,14 +146,12 @@ class Linker:
             return
 
         while True:
-            import sys
             if link.state == Link.REMOVED:
                 del self._links[linkid]
                 break
 
             try:
                 if self._heartbeat_check(link) is False:
-                    print(link.ident + " Timeout")
                     raise ConnectionError()
                 letter = await receving(reader, timeout=3)
             except (ConnectionError, ConnectionResetError, BrokenPipeError):
@@ -163,8 +161,6 @@ class Linker:
                 # Exit
                 return
             except asyncio.exceptions.TimeoutError:
-                print(link.ident + " Turn")
-                sys.stdout.flush()
                 continue
 
             if isinstance(letter, HeartbeatLetter):
@@ -313,9 +309,6 @@ class Linker:
         hb = HeartbeatLetter(self._hostname, link.hbCount)
         try:
             await sending(link.writer, hb)
-            print("SendHB " + str(hb) +  " to " + link.ident)
-            import sys
-            sys.stdout.flush()
         except ConnectionError:
             # Just return
             # that link will be rebuild while timer
