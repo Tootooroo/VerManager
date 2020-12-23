e8e50a042694633028f90c1dd76367f95db1242c
Tootooroo <aydenlin@foxmail.com>
2020-12-23 17:27:56 +0800

    Testcaess edit.

--- client/tests.py
+++ client/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
-from client.TestCases.MessageTestCases import MessageTestCaes
+from client.TestCases.MessageTestCases import MessageTestCaes, \
+    JobHistoryMessageTestCases
